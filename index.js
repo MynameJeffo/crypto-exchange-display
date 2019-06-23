a5cf12b9a4a529c7af8ac25c76f696f4d4720e9a
makebestapps@gmail.com <makebestapps@gmail.com>
2019-06-24 00:13:32 +0800

    added react front end

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 const express = require('express')
 const cowsay = require('cowsay')
 const cors = require('cors')
+const path = require('path')
 // Create the server
 const app = express()
+
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, 'client/build')))
+
+
 // Serve our api route /cow that returns a custom talking text cow
 app.get('/api/cow/:say', cors(), async (req, res, next) => {
   try {
     
     const text = req.params.say
     const moo = cowsay.say({ text })
+    console.log("untz untz")
     res.json({ moo })
   } catch (err) {
     next(err)
@@ -18,13 +25,14 @@ app.get('/api/cow/:say', cors(), async (req, res, next) => {
 app.get('/api/cow/', cors(), async (req, res, next) => {
   try {
     const moo = cowsay.say({ text: 'Hello World!' })
+    console.log("yr mum")
     res.json({ moo })
   } catch (err) {
     next(err)
   }
 })
 
-app.post('/api/currency/create/', function (req, res, next) {
+app.post('/api/currency/post/', function (req, res, next) {
     console.log("create currency");
     try {
         const moo = cowsay.say({ text: 'untz untz create the currency' })
@@ -63,6 +71,12 @@ app.get('/api/currency/get/', function (req, res, next) {
         next(err)
     }
 })
+
+// Anything that doesn't match the above, send back index.html
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/client/build/index.html'))
+})
+
 // Choose the port and start the server
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
